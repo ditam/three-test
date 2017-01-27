a72a9213b19deef1cd7fdb3885c34bccf5495653
ditam <ditam@users.noreply.github.com>
2017-01-28 00:17:17 +0100

    add keyboard input based rotation

--- three-test.js
+++ three-test.js
@@ -2,6 +2,58 @@
 var scene, camera, renderer, sideRenderer;
 var geometry, material, mesh;
 
+var KEYS = Object.freeze({
+  W:{
+    rotateX: -1,
+    rotateY: 0,
+    rotateZ: 0
+  },
+  A:{
+    rotateX: 0,
+    rotateY: -1,
+    rotateZ: 0
+  },
+  S:{
+    rotateX: 1,
+    rotateY: 0,
+    rotateZ: 0
+  },
+  D:{
+    rotateX: 0,
+    rotateY: 1,
+    rotateZ: 0
+  },
+  NONE: {
+    rotateX: 0,
+    rotateY: 0,
+    rotateZ: 0
+  }
+});
+var currentKey = KEYS.NONE;
+
+document.onkeypress = function(event) {
+  switch (event.code) {
+    case 'KeyW':
+      currentKey = KEYS.W;
+      break;
+    case 'KeyA': 
+      currentKey = KEYS.A;
+      break;
+    case 'KeyS': 
+      currentKey = KEYS.S;
+      break;
+    case 'KeyD': 
+      currentKey = KEYS.D;
+      break;
+    default:
+      currentKey = KEYS.NONE;
+  }
+}
+
+document.onkeyup = function() {
+  currentKey = KEYS.NONE;
+}
+
 function init() {
   scene = new THREE.Scene();
 
@@ -25,11 +77,12 @@ function init() {
   document.getElementById('side-renderer-target').appendChild( sideRenderer.domElement );
 }
 
+
 function animate() {
   requestAnimationFrame( animate );
 
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.02;
+  mesh.rotation.x += 0.01 * currentKey.rotateX;
+  mesh.rotation.y += 0.01 * currentKey.rotateY;
 
   renderer.render( scene, camera );
 }
